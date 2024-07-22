9f135f1e69aee0df7a40f6787fef460cc6e56f14
Scott D'Alessandro <scott@fullstackacademy.com>
2024-07-22 12:04:48 -0400

    update return type string, add invalid input check

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,4 @@
 export const fizzbuzz = (num: number):string => {
+    if(num < 1 || num > 100 ) return 'Invalid input';
     return num.toString();
 }
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,6 +6,13 @@ describe("fizzbuzz", () => {
         expect(fizzbuzz).toBeDefined();
     })
     it('returns a string', () => {
-        expect(fizzbuzz(1)).toBeInstanceOf(String);
+        expect(typeof fizzbuzz(1)).toBe('string');
     })
+
+    it('returns "Invalid input" when number is less than 1 and greater than 100', () => {
+        expect(fizzbuzz(0)).toBe('Invalid input');
+        expect(fizzbuzz(101)).toBe('Invalid input');
+        expect(fizzbuzz(-1)).toBe('Invalid input');
+    })
+
 });
