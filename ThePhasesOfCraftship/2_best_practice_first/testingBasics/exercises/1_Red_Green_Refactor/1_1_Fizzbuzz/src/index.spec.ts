ba08a16dba424e433bb6ad506f03098d04196c84
Scott D'Alessandro <scott@fullstackacademy.com>
2024-07-22 11:54:44 -0400

    define fizzbuzz and check the data type returned

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,7 +1,11 @@
-
+import { fizzbuzz } from "./fizzbuzz";
+import { describe, expect, it } from '@jest/globals';
 describe("fizzbuzz", () => {
     
     it("fizzbuzz should be defined",() => {
         expect(fizzbuzz).toBeDefined();
+    })
+    it('returns a string', () => {
+        expect(fizzbuzz(1)).toBeInstanceOf(String);
     })
 });
