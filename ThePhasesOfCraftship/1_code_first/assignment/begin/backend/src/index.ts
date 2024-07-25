867115ea44f0407bb3c2faf4af1eda3b45e809a1
Scott D'Alessandro <scott@fullstackacademy.com>
2024-07-25 13:45:04 -0400

    add prisma connection, add create user route, spaghetti code ftw!

--- ThePhasesOfCraftship/1_code_first/assignment/begin/backend/src/index.ts
+++ ThePhasesOfCraftship/1_code_first/assignment/begin/backend/src/index.ts
@@ -1,12 +1,77 @@
 import express, { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+// console.log(prisma);
+
+type User = {
+    email: string
+    username: string
+    firstName: string
+    lastName: string
+    password: string
+}
+
+const basicPasswordGenerator = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    let result = '';
+    for (let i = 0; i < 10; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
+
+const createUser = async ({ email, firstName, lastName, username }: Omit<User, 'password'>) => {
+    const user = await prisma.user.create({
+        data: {
+            email,
+            username,
+            firstName,
+            lastName,
+            password: basicPasswordGenerator()
+        }
+    })
+    return user
+}
+
 
 const app = express()
-const port = 3000
+const port = 8090
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Server is running on port ${port}`)
 })
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+// Create a new user
+app.post('/users/new', async (req: Request, res: Response) => {
+    console.log('hello!!!!')
+    console.log(req.body)
+// ...
+    const { email, firstName, lastName, username } = req.body
+    try {
+        const user = await createUser({ email, firstName, lastName, username })
+        res.status(201).send(user)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Error creating user')
+    }
+    // .... more code
+});
+  
+// Edit a user
+app.post('/users/edit/:userId', async (req: Request, res: Response) => {
+// ...
+});
+  
+// Get a user by email
+app.get('/users', async (req: Request, res: Response) => {
+// ...
+res.send('users route')
+});
+  
